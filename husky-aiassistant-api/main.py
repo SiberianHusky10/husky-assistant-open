047f00dd1d1d356f049d76d1a566995ae82ff195
SiberianHusky10 <2069506402@qq.com>
2026-03-12 23:02:49 +0800

    solve cros question

--- husky-aiassistant-api/main.py
+++ husky-aiassistant-api/main.py
@@ -12,19 +12,20 @@ import time
 from routers.voice import router as voice_router
 
 
+
 app = FastAPI()
+
+# 允许所有来源
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # 允许所有域名
+    allow_credentials=True,    # 是否允许发送 Cookie
+    allow_methods=["*"],       # 允许所有请求方法
+    allow_headers=["*"],       # 允许所有请求头
+)
 
 app.include_router(voice_router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  # 前端地址
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # 1. 定义请求体格式
 class ChatRequest(BaseModel):
