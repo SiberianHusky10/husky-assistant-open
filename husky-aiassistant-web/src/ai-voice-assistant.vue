36ddbee6ff84a5ee42372865454fdcc3ebfe4e60
SiberianHusky10 <2069506402@qq.com>
2026-03-16 22:22:54 +0800

    domain change,so  I change frontend url

--- husky-aiassistant-web/src/ai-voice-assistant.vue
+++ husky-aiassistant-web/src/ai-voice-assistant.vue
@@ -289,8 +289,8 @@ const sendMessage = async () => {
   try {
     // 调用后端接口
     // 本地测试：http://localhost:8000/chat
-    // 线上测试：https://aihusky.tech/chat
-    const response = await fetch('https://aihusky.tech/chat', {
+    // 线上测试：https://api.aihusky.tech/chat
+    const response = await fetch('https://api.aihusky.tech/chat', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
